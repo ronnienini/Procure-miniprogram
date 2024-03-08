0647ebcd5a54c76583d7c55b4ffe1ec886dcf0e7
Bin Fun <bin-binbin1>
2024-03-08 10:59:00 +0800

    server

---
+++ procureServer/src/main/java/com/procure/procureserver/ProcureServerApplication.java
@@ -0,0 +1,13 @@
+package com.procure.procureserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcureServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcureServerApplication.class, args);
+	}
+
+}
---
+++ procureServer/src/test/java/com/procure/procureserver/ProcureServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.procure.procureserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcureServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
