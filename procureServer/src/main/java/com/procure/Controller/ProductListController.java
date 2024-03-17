d4e189bdc690461488429d32599e4b9ce320a5e0
Bin Fun <bin-binbin1>
2024-03-17 15:56:53 +0800

    image

--- procureServer/src/main/java/com/procure/Controller/ProductListController.java
+++ procureServer/src/main/java/com/procure/Controller/ProductListController.java
@@ -6,15 +6,21 @@ import com.procure.Service.ProductListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 public class ProductListController {
     @Autowired
     private ProductListService service;
-    private ObjectMapper mapper = new ObjectMapper();
-    private static final String path = "C:\\pic";//Linux下需要更改
+    private final ObjectMapper mapper = new ObjectMapper();
+    private static final String path = "C:\\pic\\";//Linux下需要更改
     @GetMapping("/cate/types")
     public ResponseEntity<String> getTypes(){
         String json="";
@@ -34,6 +40,12 @@ public class ProductListController {
             e.printStackTrace();
         }
         return ResponseEntity.ok(json);
+    }
+    @GetMapping("/cate/image/{picName}")
+    public byte[] getImage(@PathVariable String picName) throws IOException {
+        Path imagePath= Paths.get(path+picName);
+        byte[] imageBytes = Files.readAllBytes(imagePath);
+        return imageBytes;
     }
 
 }
